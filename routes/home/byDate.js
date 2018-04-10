0852c1ab048096c4bb639cb16997f1e7d549f1b3
kojang <nogoto@naver.com>
2018-04-11 02:30:45 +0900

    메인홈화면 수정, date format 보완

--- routes/home/byDate.js
+++ routes/home/byDate.js
@@ -1,47 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../../config/dbpool');
-var math = require('mathjs');
+const math = require('mathjs');
+
 
 //callback함수
 const async = require('async');
-//파일 업로드용 미들웨어(S3에 업로드)
-// const multer = require('multer');
-// const multerS3 = require('multer-s3');
-// const aws = require('aws-sdk');
-// aws.config.loadFromPath('./config/aws_config.json');
-// const s3 = new aws.S3();
-// const upload = multer({
-//   storage: multerS3({
-//     s3 : s3,
-//     bucket: 'uniquename1234', //버킷 설정
-//     acl: 'public-read', //권한 설정
-//     key: function(req, file, cb) { //업로드할 파일 이름 설정
-//       cb(null, Date.now() + '.' + file.originalname.split('.').pop());//저장되는 이름 설정
-//     }
-//   })
-// });
-
+const dateFormat = require('dateformat');
 
 //랜덤 함수
 // function getRandomInt(min, max) { //min ~ max 사이의 임의의 정수 반환
 // return math.floor(math.random() * (max - min)) + min;
 // }
-
+// VOLUME /var/lib/node
+// VOLUME /root/.pm2
 /*날짜별 미션과 후기*/
 
-/*파일 업로드
-  여러 개의 파일을업로드-> array 사용, 두 번째 인자로 파일의 갯수 명시, req.files[i].location으로 링크 받아옴
-  하나의 파일 -> single, req.file.location으로 파일 링크 받아옴
-*/
-router.post('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
+
   let taskArray = [
-    (callback) => {
+    function(callback){
       pool.getConnection((err, connection) => {
         if(err) {
           connection.release();
           res.status(500).send({
-            stat:'fail'
+            stat:'connection fail'
           });
           callback("errer"+err, null);
         }else{
@@ -50,85 +33,182 @@ router.post('/', function(req, res, next) {
       });
     },
 
-    (connection, callback) => {
-      let selectquery = "select * from subject where date = ?;";
-      connection.query(selectquery,[req.body.date],(err,subject) =>{
-        if(subject[0] == null){
+    function(connection, callback) {
+      let selectquery = "select * from subject order by subject_id limit 7;";
+      connection.query(selectquery,[], (err, subjects) =>{
+        if(err){
+          res.status(500).send({
+            stat: 'select query fail'
+          })
+          return callback(err);
+        }
+        else if(!subjects.length){
           res.status(501).send({
-            stat:'no contents'
+            stat: 'no subjects'
           });
-          connection.release();
-          callback("error"+err, null);
-        }else{
-          callback(null, connection, subject);
+        }
+        else
+        {
+          console.log(subjects);
+          callback(null, connection, subjects, callback);
         }
       });
     },
 
-    (connection, subject, callback) => {
-      let countquery = "select count(review_id) as cnt from review where subject_id = ?;";
-      connection.query(countquery,[subject[0].subject_id],(err,count) =>{
-        if(count[0].cnt == 0){ //후기 없음
-          res.status(502).send({
-            stat:'no reviews'
-          });
-          connection.release();
-          callback("errer"+err, null);
-        }else{
-          callback(null, connection, subject, count);
-        }
-      });
-    },
+    function(connection, subjects, callback){
+      console.log('second callback');
+      var bundles = [];
 
-    (connection, subject, count, callback) => {
-      var info = new Array(10);
-      var num = new Array(count[0].cnt);
-      let selectquery = "select user.id, user.nickname, user.imgLink, user.level, review.comment, review.date from user inner join review on user.user_id = review.user_id where subject_id = ?;";
-      connection.query(selectquery,[subject[0].subject_id],(err,rows) =>{
-        if(count[0].cnt < 10){ //1~10 보내줌
-          for(let i=0; i < count[0].cnt; i++){
-            info[i] = rows[i];
-          }
-          callback(null, connection, subject, info, rows, num);
-        }else{//랜덤으로 10개의 숫자 뽑아서 보내줌
-          for(let i=0; i < 10; i++){
-            num[i] = Math.floor(Math.random() * (count[0].cnt-1));
-            //getRandomInt(0, count.cnt-1);
+      var load = function(index){  //load 재귀 호출 정의
 
-            //중복체크
-            for(var j=0; j<i; j++){
-              if(num[i] == num[j]){
-                  i = i - 1;
-                  break;
-              }
-            }
+        var subject = subjects[index]; //하나 추출
+        var reviews = [];
+        console.log(subject);
 
-          }
+        //var count = checkcount(subject);
 
-          for(let i = 0; i < 10; i++){
-            info[i] = rows[num[i]];
-          }
-          callback(null, connection, subject, info, rows, num);
-        }
-      });
-    },
+        // var info = new Array(10);
+        //
+        //    var num = new Array(count);
+         var info = [];
+         var num = [];
 
-    (connection, subject, info, rows, num, callback) => {
-      res.status(201).send({
-        stat:"success",
-        data:{
-          "subject": subject[0],
-          "comments" : info
-        }
-      });
-      connection.release();
-      callback(null, null);
-    }
-  ];
+         var countquery = "select count from reviewcount where subject_id = ?";
+         connection.query(countquery, [subject.subject_id], (err, count) =>{
+           console.log('checkcount query');
+           if(err){
+               console.log('checkcount error');
+             res.status(500).send({
+               stat: 'select query fail'
+             });
+             connection.release();
+             callback("errer"+err, null);
+
+           }
+           else if(count[0] == 0)
+           {
+             console.log('checkcount 0');
+
+             var bundle = {
+               subject: {
+                 'subject_id': subjects[index].subject_id,
+                 'contents': subjects[index].contents,
+                 'date' : dateFormat(subjects[index].date, 'yyyy-mm-dd')
+               },
+               reviews: reviews
+             };
+
+             bundles.push(bundle);
+
+             load(index + 1);
+           }
+           else{
+
+               let selectquery = "select user.id, user.nickname, user.imgLink, user.level, review.comment, review.date from user inner join review on user.user_id = review.user_id where subject_id = ?;";
+               console.log('subject id: '+ subject.subject_id);
+
+               var count = count[0].count;
+               console.log(count);
+               connection.query(selectquery, [subject.subject_id],(err,rows) =>{
+                 console.log(rows);
+
+                 if(count < 10){ //10보다 작으면 갯수만큼 보내줌
+
+                   for(let i=0; i < count; i++){
+                     //info.push(rows[i]);
+                     info[i] = rows[i];
+                   }
+                 }
+                 else{     //랜덤으로 10개의 숫자 뽑아서 보내줌 10개보다 이상일때
+                   for(let i=0; i < count; i++){
+                     num[i] = Math.floor(Math.random() * (count-1));
+                     //getRandomInt(0, count.cnt-1);
+                     //중복체크
+                     for(var j=0; j<i; j++){
+                       if(num[i] == num[j]){
+                           i = i - 1;
+                           break;
+                       }
+                     }
+                   }
+
+                   for(let i = 0; i < count; i++){
+                     info[i] = rows[num[i]];
+                   }
+
+                 }
+                 console.log(info);
+
+
+
+                 for(let i =0; i < count; i++){
+                    var tempinfo = info[i];
+
+                    var user ={
+                        'id': tempinfo.id,
+                        'nickname': tempinfo.nickname,
+                        'imgLink': tempinfo.imgLink,
+                        'level': tempinfo.level
+                    };
+
+                    var review = {
+                      'comment': tempinfo.comment,
+                      'date': dateFormat(tempinfo.date, 'yyyy-mm-dd')
+                    };
+
+                    var bundle = {
+                      'user': user,
+                      'review': review,
+                    }
+
+                    reviews.push(bundle);
+
+                 }
+
+                 var bundle = {
+                   subject: {
+                     'subject_id': subjects[index].subject_id,
+                     'contents': subjects[index].contents,
+                     'date' : dateFormat(subjects[index].date, 'yyyy-mm-dd')
+                   },
+                   reviews: reviews
+                 };
+
+                 bundles.push(bundle);
+
+
+                 if(!subjects[index+1])
+                 {
+                   res.status(201).send({
+                     stat: 'success',
+                     data: bundles
+                   });
+
+                   connection.release();
+
+                 }
+                 else{
+                   load(index + 1);
+                 }
+
+               });
+           }
+         });
+       };
+
+         if(subjects.length>0){
+           console.log('재귀시작');
+           load(0);
+
+           console.log('재귀끝');
+         }
+
+       }
+      ]
 
   async.waterfall(taskArray, (err, result) => {
      if(err) console.log(err);
-     else console.log(result);
+     else console.log('done');
   });
 });
 
