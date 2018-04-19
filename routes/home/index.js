4714a4541ddc7430b55bf797020bf0759738e835
kojang <nogoto@naver.com>
2018-04-19 18:58:08 +0900

    index.js 수정 불필요한 api todayS.js 삭제

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var todayS = require('./todayS');
+
 var byDate = require('./byDate');
 var writeRev = require('./writeRev');
 
 
-router.use('/todayS',todayS);
+
 router.use('/byDate',byDate);
 router.use('/writeRev',writeRev);
 
